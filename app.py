ef46ccd96f1b2b14127ce542476f5acc72e70e18
unnath0 <timeandwind01@gmail.com>
2024-01-16 19:41:04 +0530

    modified:   app.py 	modified:   static/dist/css/output.css 	new file:   static/src/css/visualization.css 	modified:   templates/dashboard.html 	modified:   templates/visualization.html 	modified:   utils/__pycache__/attendance.cpython-311.pyc 	modified:   utils/__pycache__/ia.cpython-311.pyc 	modified:   utils/attendance.py 	modified:   utils/ia.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import mysql.connector
 
 # importing visualization related modules
 import pandas as pd
-# from utils import attendance as attend
-# from utils import ia as ia_marks
+from utils import attendance as attend
+from utils import ia as ia_marks
 from utils import csv_to_database_tables as csvToDB
 
 
@@ -25,6 +25,9 @@ db_connection = mysql.connector.connect(
 # Create a cursor to interact with the database
 cursor = db_connection.cursor()
 
+filePaths = []
+tableNames = []
+
 @app.route('/')
 @app.route('/home')
 def home():
@@ -33,19 +36,20 @@ def home():
 @app.route('/dashboard/', methods=['GET','POST'])
 def dashboard():
     if request.method == 'POST':
-        print("working")
         csv_file = request.files['classCsvFile']
-
         csv_file.save(os.path.join('static', csv_file.filename))
-
         csv_file_path = os.path.join('static',csv_file.filename)
-
         table_name = csvToDB.studentTableCreation(csv_file_path, db_connection)
+
+        filePaths.append(csv_file_path)
+        tableNames.append(table_name)
+        print(tableNames, "\n", tableNames[0])
 
         # get table details to show to user        
         # query = f"SELECT * FROM `{table_name}`"
         # cursor.execute(query)
-        # Fetch data for all students
+
+        # Fetch attendance data for all students
         cursor.execute(f"SELECT `SR. NO`, `STUDENTS NAME`, `ATTENDANCE PERCENTAGE`, `TOTAL HELD`, `TOTAL PRESENT` FROM `{table_name}`")
         tableDisplayedToUser = cursor.fetchall()
 
@@ -55,7 +59,11 @@ def dashboard():
 
 @app.route('/visualization')
 def visualization():
-    return render('visualization.html')
+    print(tableNames)
+    attendanceAnalytics = attend.runAll(tableNames[0], db_connection, cursor)
+    iaMarksAnalytics = ia_marks.runAll(tableNames[0],db_connection)
+
+    return render('visualization.html', attendanceAnalytics = attendanceAnalytics, iaMarksAnalytics = iaMarksAnalytics)
 
 
 if __name__ == '__main__':
--- utils/attendance.py
+++ utils/attendance.py
@@ -1,9 +1,5 @@
-import os
 import pandas as pd
-import matplotlib.pyplot as plt
-import plotly
 import plotly.express as px
-import seaborn as sns
 import plotly.graph_objects as go
 import mysql.connector
 
@@ -13,159 +9,111 @@ def createDataframe(table_name, db_connection):
     df = pd.read_sql(query, con=db_connection)
     return df
 
-def createAnalysisFolder():
-    # Specify the destination folder
-    destination_path = os.path.join(settings.BASE_DIR, 'global/analysis/attendance')
 
-    # Ensure the destination folder exists, create it if not
-    os.makedirs(destination_path, exist_ok=True)
+def createAnalytics(df, table_name, cursor):
+    attendanceAnalytics = []
 
-    return destination_path
-
-def attendanceCategories(df, destination_path, visualizations_path):
     # Assuming df is your DataFrame with 'ATTENDANCE PERCENTAGE' column
-    labels = ['<75%', '75-85%', '>85%']
-    attendance_bins = [0, 75, 85, 100]
-    attendance_categories = pd.cut(df['ATTENDANCE PERCENTAGE'], bins=attendance_bins, labels=labels)
+    labels_categories = ['<75%', '75-85%', '>85%']
+    attendance_bins_categories = [0, 75, 85, 100]
+    attendance_categories = pd.cut(df['ATTENDANCE PERCENTAGE'], bins=attendance_bins_categories, labels=labels_categories)
 
-    # Pie Chart for Number of Students in Each Attendance Category
-    pie_chart_attendance_categories = px.pie(names=attendance_categories, title='Pie Chart for Attendance Categories')
-    # pie_chart_attendance_categories.show()
-    plotly.offline.plot(pie_chart_attendance_categories, filename=f"{destination_path}/pieChartCategories.html", auto_open=False)
-    visualizations_path['CategoriesPie'] = f"{destination_path}/pieChartCategories.html"
+    # Create visually appealing pie chart for attendance categories using Plotly Express
+    pie_chart_attendance_categories = px.pie(
+        names=attendance_categories,
+        title='Pie Chart for Attendance Categories',
+        color_discrete_sequence=px.colors.qualitative.Plotly,  # Set color sequence
+    )
 
-def pieChart_AttendanceStatus(df, destination_path, visualizations_path):
+    # Update layout for a visually appealing pie chart
+    pie_chart_attendance_categories.update_layout(
+        margin=dict(l=20, r=20, t=50, b=20),  # Adjust margins for better layout
+        paper_bgcolor='rgba(0,0,0,0)',  # Make the background transparent
+        plot_bgcolor='rgba(0,0,0,0)',  # Make the plot area background transparent
+    )
+
+    # Create bar chart for attendance status using Plotly Graph Objects
     attendance_status_labels = ['Low Attendance', 'Moderate Attendance', 'High Attendance']
     attendance_status_bins = [0, 75, 90, 100]
     attendance_status_counts = pd.cut(df['ATTENDANCE PERCENTAGE'], bins=attendance_status_bins, labels=attendance_status_labels).value_counts()
 
-    pie_chart_attendance_status = px.pie(names=attendance_status_counts.index, values=attendance_status_counts.values, title='Pie Chart for Class Attendance Status')
-    # pie_chart_attendance_status.show()
-    plotly.offline.plot(pie_chart_attendance_status, filename=f"{destination_path}/pieChartStatus.html", auto_open=False)
-    visualizations_path['StatusPie'] = f"{destination_path}/pieChartStatus.html"
+    bar_chart_attendance_status = go.Figure(
+        data=[go.Bar(x=attendance_status_counts.index, y=attendance_status_counts.values)],
+    )
 
-def attendanceBarChart(df, destination_path, visualizations_path):
-    # Assuming df is your DataFrame
-    bar_chart = px.bar(df, x='STUDENTS NAME', y='ATTENDANCE PERCENTAGE',
-                    title='Bar Chart for Attendance',
-                    labels={'ATTENDANCE PERCENTAGE': 'Attendance Percentage'},
-                    color='STUDENTS NAME')
+    # Update layout for the bar chart
+    bar_chart_attendance_status.update_layout(
+        title='Bar Chart for Class Attendance Status',
+        xaxis_title='Attendance Status',
+        yaxis_title='Number of Students',
+        margin=dict(l=20, r=20, t=50, b=20),  # Adjust margins for better layout
+        paper_bgcolor='rgba(0,0,0,0)',  # Make the background transparent
+        plot_bgcolor='rgba(0,0,0,0)',  # Make the plot area background transparent
+    )
 
-    # bar_chart.show()
-    plotly.offline.plot(bar_chart, filename=f"{destination_path}/AllStudentsAtOnce.html", auto_open=False)
-    visualizations_path['AllAtOnceBar'] = f"{destination_path}/AllStudentsAtOnce.html"
+    # Perform data analysis and create the additional pie chart
+    attendance_status_labels = ['Low Attendance', 'Moderate Attendance', 'High Attendance']
+    attendance_status_bins = [0, 75, 90, 100]
+    attendance_status_counts = pd.cut(df['ATTENDANCE PERCENTAGE'], bins=attendance_status_bins, labels=attendance_status_labels).value_counts()
 
-def attendanceDistributionHistogram(df, destination_path, visualizations_path):
-    histogram_attendance_distribution = px.histogram(df, x='ATTENDANCE PERCENTAGE', title='Attendance Distribution Histogram', nbins=20)
-    # histogram_attendance_distribution.show()
-    plotly.offline.plot(histogram_attendance_distribution, filename=f"{destination_path}/DistributionHistogram.html", auto_open=False)
-    visualizations_path['DistributionHistogram'] = f"{destination_path}/DistributionHistogram.html"
+    # Create additional pie chart using Plotly Graph Objects
+    pie_chart_attendance_status = go.Figure(
+        data=[go.Pie(labels=attendance_status_counts.index, values=attendance_status_counts.values)],
+    )
 
-def barPlotForAbsentees(df, destination_path, visualizations_path):
-    # Bar plot of Absent students
-    bar_fig = px.bar(df, x='STUDENTS NAME', y='ABSENT', title='Number of Classes Absent')
-    bar_fig.update_layout(xaxis=dict(tickangle=45))
-    plotly.offline.plot(bar_fig, filename=f"{destination_path}/Absentees.html", auto_open=False)
-    visualizations_path['AbsenteesBar'] = f"{destination_path}/Absentees.html"
+    # Update layout for the additional pie chart
+    pie_chart_attendance_status.update_layout(
+        title='Pie Chart for Class Attendance Status',
+        margin=dict(l=20, r=20, t=50, b=20),  # Adjust margins for better layout
+        paper_bgcolor='rgba(0,0,0,0)',  # Make the background transparent
+        plot_bgcolor='rgba(0,0,0,0)',  # Make the plot area background transparent
+    )
 
-def attendanceBelowCertainNumbers(df, destination_path, visualizations_path):
-    # Create a table with USN, student names, attendance percentage, total classes held, and total classes attended
-    table_fig = go.Figure(data=[go.Table(
-        header=dict(values=['USN/Sr.no', 'Students Name', 'Attendance Percentage', 'Total Classes Held', 'Total Classes Attended']),
-        cells=dict(values=[df['SR. NO'], df['STUDENTS NAME'], df['ATTENDANCE PERCENTAGE'],df['TOTAL HELD'], df['TOTAL PRESENT'] ])
-    )])
+    # Create histogram for attendance distribution using Plotly Express
+    histogram_attendance_distribution = px.histogram(
+        df,
+        x='ATTENDANCE PERCENTAGE',
+        title='Attendance Distribution Histogram',
+        nbins=20,
+    )
 
-    # Update layout
-    table_fig.update_layout(title_text='Students Attendance Table')
+    # Update layout for the histogram
+    histogram_attendance_distribution.update_layout(
+        margin=dict(l=20, r=20, t=50, b=20),  # Adjust margins for better layout
+        paper_bgcolor='rgba(0,0,0,0)',  # Make the background transparent
+        plot_bgcolor='rgba(0,0,0,0)',  # Make the plot area background transparent
+    )
+
+    # Save the plots as HTML
+    pie_chart_html = pie_chart_attendance_categories.to_html(full_html=False)
+    bar_chart_html = bar_chart_attendance_status.to_html(full_html=False)
+    additional_pie_chart_html = pie_chart_attendance_status.to_html(full_html=False)
+    histogram_html = histogram_attendance_distribution.to_html(full_html=False)
+
+    attendanceAnalytics.extend([pie_chart_html, bar_chart_html, additional_pie_chart_html, histogram_html])
+
+    # tables below
+
+    # Fetch data for all students
+    cursor.execute(f"SELECT `SR. NO`, `STUDENTS NAME`, `ATTENDANCE PERCENTAGE`, `TOTAL HELD`, `TOTAL PRESENT` FROM `{table_name}`")
+    data = cursor.fetchall()
+
+    # Fetch data for students with attendance less than 85%
+    cursor.execute(f"SELECT `SR. NO`, `STUDENTS NAME`, `ATTENDANCE PERCENTAGE`, `TOTAL HELD`, `TOTAL PRESENT` FROM `{table_name}` WHERE `ATTENDANCE PERCENTAGE` < 85")
+    data_below_85 = cursor.fetchall()
+
+    # Fetch data for students with attendance less than 75%
+    cursor.execute(f"SELECT `SR. NO`, `STUDENTS NAME`, `ATTENDANCE PERCENTAGE`, `TOTAL HELD`, `TOTAL PRESENT` FROM `{table_name}` WHERE `ATTENDANCE PERCENTAGE` < 75")
+    data_below_75 = cursor.fetchall()
+
+    attendanceAnalytics.extend([data, data_below_75, data_below_85])
+
+    return attendanceAnalytics
 
 
-    # Filter students with attendance below 75%
-    below_75_df = df[df['ATTENDANCE PERCENTAGE'] < 75]
 
-    # Filter students with attendance below 85%
-    below_85_df = df[df['ATTENDANCE PERCENTAGE'] < 85]
-
-    # Create a table for students with attendance below 75%
-    table_below_75 = go.Figure(data=[go.Table(
-        header=dict(values=['USN/SR. NO', 'Students Name', 'Attendance Percentage', 'Total Classes Held', 'Total Classes Attended']),
-        cells=dict(values=[below_75_df['SR. NO'], below_75_df['STUDENTS NAME'], below_75_df['ATTENDANCE PERCENTAGE'],
-                        below_75_df['TOTAL HELD'], below_75_df['TOTAL PRESENT']])
-    )])
-
-    # Update layout
-    table_below_75.update_layout(title_text='Students with Attendance Below 75%')
-
-    # Create a table for students with attendance below 85%
-    table_below_85 = go.Figure(data=[go.Table(
-        header=dict(values=['USN/SR. NO', 'Students Name', 'Attendance Percentage', 'Total Classes Held', 'Total Classes Attended']),
-        cells=dict(values=[below_85_df['SR. NO'], below_85_df['STUDENTS NAME'], below_85_df['ATTENDANCE PERCENTAGE'],
-                        below_85_df['TOTAL HELD'], below_85_df['TOTAL PRESENT']])
-    )])
-
-    # Update layout
-    table_below_85.update_layout(title_text='Students with Attendance Below 85%')
-
-    # Show the table
-    # table_fig.show()
-    # table_below_75.show()
-    # table_below_85.show()
-    plotly.offline.plot(table_fig, filename=f"{destination_path}/AllStudentsPercentages.html", auto_open=False)
-    plotly.offline.plot(table_below_75, filename=f"{destination_path}/Below75.html", auto_open=False)
-    plotly.offline.plot(table_below_85, filename=f"{destination_path}/Below85.html", auto_open=False)
-    visualizations_path['AllStudentsTable'] = f"{destination_path}/AllStudentsPercentages.html"
-    visualizations_path['StudentsBelow75'] = f"{destination_path}/Below75.html"
-    visualizations_path['StudentsBelow85'] = f"{destination_path}/Below85.html"
-
-def pieChartForAttendanceRanges(df, destination_path, visualizations_path):
-    # Assuming df is your DataFrame with 'ATTENDANCE PERCENTAGE' column
-    labels = ['<75%', '75-80%', '80-85%', '85-90%', '90-95%', '95-100%']
-    attendance_ranges = pd.cut(df['ATTENDANCE PERCENTAGE'], bins=[0, 75, 80, 85, 90, 95, 100], labels=labels)
-
-    pie_chart_attendance_ranges = px.pie(names=attendance_ranges, title='Pie Chart for Attendance Ranges')
-    # pie_chart_attendance_ranges.show()
-    plotly.offline.plot(pie_chart_attendance_ranges, filename=f"{destination_path}/pieChartRanges.html", auto_open=False)
-    visualizations_path['RangesPie'] = f"{destination_path}/pieChartRanges.html"
-
-def classAttendanceStatistics(df, destination_path, visualizations_path):
-    class_attendance_stats = df['ATTENDANCE PERCENTAGE'].describe()
-    class_attendance_stats_chart = px.bar(x=class_attendance_stats.index, y=class_attendance_stats.values,
-                                        labels={'index': 'Statistic', 'value': 'Attendance Percentage'},
-                                        title='Class Attendance Statistics')
-    # class_attendance_stats_chart.show()
-    plotly.offline.plot(class_attendance_stats_chart, filename=f"{destination_path}/ClassStatistics.html", auto_open=False)
-    visualizations_path['ClassStatisticsBar'] = f"{destination_path}/ClassStatistics.html"
-
-def runAll(table_name, db_connection):
+def runAll(table_name, db_connection, cursor):
     df = createDataframe(table_name, db_connection)
-    destination_path = createAnalysisFolder()
-    visualizations_path = {}
-    attendanceCategories(df, destination_path, visualizations_path)
-    pieChart_AttendanceStatus(df, destination_path, visualizations_path)
-    attendanceBarChart(df, destination_path, visualizations_path)
-    attendanceDistributionHistogram(df, destination_path, visualizations_path)
-    barPlotForAbsentees(df, destination_path, visualizations_path)
-    attendanceBelowCertainNumbers(df, destination_path, visualizations_path)
-    pieChartForAttendanceRanges(df, destination_path, visualizations_path)
-    classAttendanceStatistics(df, destination_path, visualizations_path)
-    return visualizations_path
+    attendanceAnalytics = createAnalytics(df, table_name, cursor)
 
-
-# Below code is just for testing whether the functions work
-
-# df = createDataframe()
-
-# attendanceCategories(df)
-
-# pieChart_AttendanceStatus(df)
-
-# attendanceBarChart(df)
-
-# attendanceDistributionHistogram(df)
-
-# barPlotForAbsentees(df)
-
-# attendanceBelowCertainNumbers(df)
-
-# pieChartForAttendanceRanges(df)
-
-# classAttendanceStatistics(df)
+    return attendanceAnalytics
